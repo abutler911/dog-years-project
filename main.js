423d032ab59de96b4e5b3f4b7ef6a4078ecb5182
Andy Butler <abutler911@gmail.com>
2022-07-27 09:11:10 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,29 @@
+//declare and assign the value 45 to a variable called myAge 
+const myAge = 45;
+//declare and assign the value 2 to a variable called earlyYears 
+
+let earlyYears = 2;
+//declare the variable laterYears and assign it the value of myAge - 2 
+
+let laterYears = myAge - 2;
+
+//multiple the value of laterYears by 4 and assign it to laterYears
+laterYears *= 4;
+
+//create a variable and assign it the value or earlyYears + laterYears
+const myAgeInDogYears = earlyYears + laterYears;
+
+//print the result to the console
+console.log(myAgeInDogYears);
+
+//declare the variable myNAme and assign it my name and then convert it to all lowercase letters
+const myName = ('Andy').toLowerCase();
+
+//print the string template literal to the console with the ages 
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+
+
+
+
+
+
